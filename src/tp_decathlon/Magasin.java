8f5a446b5cdbdb465e85c81f0e17c58ed7e43600
PA062 <PA062@10.1.62.74>
2023-11-13 00:01:58 +0100

    Méthode Magasin et ajout de commentaires

--- src/tp_decathlon/Magasin.java
+++ src/tp_decathlon/Magasin.java
@@ -6,18 +6,27 @@ public class Magasin {
     
     // Attributs
     
-    private String nom;                 // Nom du magasin
-    private int nbEqpmt;                // 
-    private String[] lstEqpmt;
-    private int nbCmd;
-    private String[] lstCmd;
+    private String nom_mag;                 // Nom du magasin
+    private final int nbEqpmt = 500;    // Nombre d'équipement vendus au magasin 
+    private String[] lstEqpmt;          // Ensemble des équipements vendus au magasin
+    private final int nbCmd = 500;      // Nombre de commandes passées depuis le magasin
+    private String[] lstCmd;            // Ensemble des commandés passées au magasin
     
     // Constructeur
     
-    public Magasin(){
+    public Magasin(String nom_mag, int nbEqpmt, String[] lstEqpmt, int nbCmd, String[] lstCmd){
+    
+        this.nom_mag = nom_mag;
+        this.lstEqpmt = lstEqpmt;
+        this.lstCmd = lstCmd;
+          
+    }
+    
+    public String toString(){
         
         
         
+        return nom_mag + " " + lstEqpmt + " " + lstCmd ;
     }
     
 }
