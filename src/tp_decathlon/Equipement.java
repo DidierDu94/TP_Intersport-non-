9ef6b6215cdeeb959a4e8eb51eb052fd59e790f1
PA062 <PA062@10.1.62.74>
2023-11-13 22:59:42 +0100

    Question 2 et avancées mineures

--- src/tp_decathlon/Equipement.java
+++ src/tp_decathlon/Equipement.java
@@ -30,6 +30,13 @@ public class Equipement {       // Classe mère
         
         return ref + " " + sport + " " + nom + " " + prix + " " + nbExmpl;
     }
+    
+    public boolean placeApres(Equipement otherEquipement){ // On veut retourner un boolean selon l'emplacement de l'équipement dans la liste. Sa place est choisie avec sa référence
+        
+        // Equipement.ref.compareTo(otherEquipement.ref)>0;
+        return true;
+        //return false;    
+    }
    
     
 }
