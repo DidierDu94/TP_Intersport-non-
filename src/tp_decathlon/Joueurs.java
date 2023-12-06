2c58840d7e3ae58844aa3416391e3d051ff452e6
i_am <i_am@10.1.63.129>
2023-12-06 18:45:17 +0100

    Commit du 06/12 à 18:45 : full bogues

--- src/tp_decathlon/Joueurs.java
+++ src/tp_decathlon/Joueurs.java
@@ -3,29 +3,27 @@ package tp_decathlon;
 // @author Calmet Pierre && Bertin Pierre-Aloïs
 // TDTP3: Vente d'équipements sportifs
 
-public class Joueurs extends Equipement{        // Super classe de ProtectionJoueurs
+public class Joueurs extends Equipement{  // Super classe de ProtectionJoueurs
     
     //Attributs
     
-    private String taille;              // Taille de l'équipement : 3XS / XXS / XS / S / M / M/L / L / XL / XXL / 3XL / 4XL / 5XL
-    private String coloris;             // Coloris de l'équipement (ex : bleu, rouge, transparent...)
+    private String taille;   // Taille de l'équipement : 3XS / XXS / XS / S / M / M/L / L / XL / XXL / 3XL / 4XL / 5XL
+    private String coloris; // Coloris de l'équipement (ex : bleu, rouge, transparent...)
     
     //Constructeur
     
-    public Joueurs(String ref, String sport, String nom, float prix, int nbExmpl, String taille, String coloris){
+    public Joueurs(String ref, String sport, String nom, float prix, int nbExmpl
+                   , String taille, String coloris){
         
         super(ref, sport, nom, prix, nbExmpl);
-        
         this.taille = taille;
-        this.coloris = coloris;
-          
+        this.coloris = coloris;      
     }
     
     // Méthode toString()
     
     @Override
     public String toString(){
-        
         return super.toString() + " " + taille + " " + coloris;   
     }
     
@@ -39,11 +37,13 @@ public class Joueurs extends Equipement{        // Super classe de ProtectionJou
         return coloris;
     }
     
-    // Méthode retournant les informations à écrire dans le fichier en String
+    /*
+    Méthode retournant sous la forme d'une chaine de caractères les 
+    informations à écrire dans le fichier texte.
+    */
     
     @Override
-    public String versFichier(){
+    public String versFichier(){  
         return super.versFichier() + " : " + taille + " : " + coloris;
-    }
-    
+    }   
 }
